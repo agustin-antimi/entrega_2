9c9f14bad50bec189f12a4b63806c5766696c084
Antimi Agustin <antimiagustin@gmail.com>
2026-03-24 12:55:45 -0300

    Fix, arreglamos la funcion en el archivo src/functions_07.py, haciendo que la copia de amigos que utilizaba un shuffle ahora sea una lsiat, para evitar errores

--- src/functions_07.py
+++ src/functions_07.py
@@ -1,6 +1,7 @@
 import random
 
-def definir_amigos_invisibles(lista:list) -> dict:
+
+def definir_amigos_invisibles(lista: list) -> dict:
     """
     Recibe un str de elementos, separados por coma, y devuelve un diccionario
     donde cada elemento le pertenece a otro sin repetirse, y sin que un elemento
@@ -10,14 +11,19 @@ def definir_amigos_invisibles(lista:list) -> dict:
     amigos = set(lista.split(","))
     amigos = {amigo.lower().rstrip(".,") for amigo in amigos}
 
+    # Un return temprano en caso de no haber mas de 3 elementos
+    if len(amigos) < 3:
+        print("Debe haber al menos 3 amigos")
+        return
+
     # Creamos una copia y los mezclamos
-    amigos_mezclados = amigos.copy()
+    amigos_mezclados = list(amigos)
     random.shuffle(amigos_mezclados)
 
     # Creamos un diccionario vacio donde guardaremos las asignaciones
     amigos_invisibles = dict()
 
-    # iteramos 
+    # iteramos
     for i in range(len(amigos_mezclados)):
         # La key sera el amigo mezcaldo
         key = amigos_mezclados[i]
@@ -28,4 +34,4 @@ def definir_amigos_invisibles(lista:list) -> dict:
         # Asignamos la key y el value a nuestro diccionario vacio
         amigos_invisibles[key] = value
 
-    return amigos_invisibles    
+    return amigos_invisibles
